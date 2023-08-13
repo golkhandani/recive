545fd93233a5dd1d65201ec5c7c97f390841a8c3
golkhandani <site.golkhandani@gmail.com>
2023-08-12 22:31:00 -0700

    007 - fix location issue

--- lib/features/near_me_page/near_me_screen.dart
+++ lib/features/near_me_page/near_me_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:flutter_map_supercluster/flutter_map_supercluster.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_switch/flutter_switch.dart';
+import 'package:flutter_use_geolocation/flutter_use_geolocation.dart';
+import 'package:location/location.dart';
 import 'package:recive/components/sliver_gap.dart';
 import 'package:recive/components/title_header.dart';
 import 'package:recive/extensions/string_extensions.dart';
@@ -55,6 +58,29 @@ class NearMeScreen extends StatefulHookWidget {
 
 class _NearMeScreenState extends State<NearMeScreen>
     with TickerProviderStateMixin {
+  final Location location = Location();
+  bool? _serviceEnabled;
+  Future<void> _checkService() async {
+    final serviceEnabledResult = await location.serviceEnabled();
+    setState(() {
+      _serviceEnabled = serviceEnabledResult;
+    });
+  }
+
+  Future<void> _requestService(
+      {required VoidCallback onGrantedPermission}) async {
+    if (_serviceEnabled ?? false) {
+      return;
+    }
+
+    final serviceRequestedResult = await location.requestService();
+    setState(() {
+      _serviceEnabled = serviceRequestedResult;
+    });
+    print("TESTED ${serviceRequestedResult}");
+    onGrantedPermission();
+  }
+
   @override
   Widget build(BuildContext context) {
     final switchIndex = useState(0);
@@ -67,15 +93,30 @@ class _NearMeScreenState extends State<NearMeScreen>
       curve: Curves.easeInOut,
     );
 
+    useEffect(() {
+      _requestService(onGrantedPermission: () {
+        // location.onLocationChanged.listen(
+        //   (location) => {
+        //     if (location.latitude != null && location.longitude != null)
+        //       {
+        //         mapController.animateTo(
+        //           dest: LatLng(location.latitude!, location.longitude!),
+        //         )
+        //       }
+        //   },
+        // );
+      });
+      return;
+    }, []);
+
     final points = <LatLng>[
-      LatLng(51.509324, -0.128128),
-      LatLng(51.502334, -0.128528),
-      LatLng(51.503214, -0.128128),
-      LatLng(51.504564, -0.128228),
-      LatLng(51.5055264, -0.125928),
-      LatLng(51.501164, -0.128428),
-      LatLng(51.502464, -0.128228),
-      LatLng(51.504564, -0.128328),
+      LatLng(37.725834, -122.416417),
+      LatLng(37.785834, -122.416417),
+      LatLng(37.785834, -122.426417),
+      LatLng(37.755834, -122.406417),
+      LatLng(37.715834, -122.416417),
+      LatLng(37.731834, -122.436417),
+      LatLng(37.742834, -122.426417),
     ];
 
     return ColoredBox(
@@ -142,6 +183,7 @@ class _NearMeScreenState extends State<NearMeScreen>
                           listSectionHeight: listSectionHeight,
                           mapController: mapController,
                           items: points,
+                          location: location,
                         ),
                       ],
                     ),
@@ -196,6 +238,15 @@ class _NearMeScreenListViewContent extends HookWidget {
   }
 }
 
+extension PositionToLatLon on GeolocationState {
+  LatLng? get latLng => position != null
+      ? LatLng(
+          position!.latitude,
+          position!.longitude,
+        )
+      : null;
+}
+
 class _NearMeScreenMapViewContent extends HookWidget {
   const _NearMeScreenMapViewContent({
     required this.items,
@@ -204,6 +255,7 @@ class _NearMeScreenMapViewContent extends HookWidget {
     required this.mapSectionHeight,
     required this.listSectionHeight,
     required this.mapController,
+    required this.location,
   });
 
   final ValueNotifier<int> switchIndex;
@@ -211,11 +263,43 @@ class _NearMeScreenMapViewContent extends HookWidget {
   final double mapSectionHeight;
   final double listSectionHeight;
   final AnimatedMapController mapController;
+  final Location location;
   final List<LatLng> items;
+
+  Marker _createMarker(LatLng point, Color color) => Marker(
+        anchorPos: AnchorPos.align(AnchorAlign.top),
+        rotateAlignment: AnchorAlign.top.rotationAlignment,
+        height: 50,
+        width: 50,
+        point: point,
+        rotate: true,
+        builder: (ctx) => Icon(
+          Icons.pin_drop,
+          color: color,
+          size: 50,
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
-    final center = useState(LatLng(51.509364, -0.128928));
+    final defaultPosition = const LatLng(51.509364, -0.128928);
+    final geolocation = useGeolocation();
+    LatLng? ltlg;
+    if (geolocation.position != null) {
+      ltlg = LatLng(
+        geolocation.position!.latitude,
+        geolocation.position!.longitude,
+      );
+    }
+
+    // final user = useState(ltlg);
+    final center = useState(
+      items.isNotEmpty ? items.first : (ltlg ?? defaultPosition),
+    );
+    final zoom = useState(15.0);
+
+    final markersA =
+        items.map((point) => _createMarker(point, Colors.blue)).toList();
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
       sliver: MultiSliver(
@@ -242,9 +326,11 @@ class _NearMeScreenMapViewContent extends HookWidget {
                               mapController: mapController.mapController,
                               options: MapOptions(
                                 center: center.value,
-                                zoom: 15,
+                                zoom: zoom.value,
                                 adaptiveBoundaries: false,
                                 keepAlive: true,
+                                maxZoom: 17,
+                                minZoom: 10,
                               ),
                               nonRotatedChildren: [
                                 RichAttributionWidget(
@@ -263,22 +349,44 @@ class _NearMeScreenMapViewContent extends HookWidget {
                                   urlTemplate:
                                       'https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=J4ktALZX8GCz9Hw7i0tK',
                                 ),
-                                MarkerLayer(
-                                  markers: [
-                                    ...List.generate(
-                                      items.length,
-                                      (index) => Marker(
-                                        point: items[index],
+                                if (geolocation.latLng != null)
+                                  MarkerLayer(
+                                    markers: [
+                                      Marker(
+                                        point: geolocation.latLng!,
                                         width: 80,
                                         height: 80,
-                                        builder: (context) => const Icon(
-                                          Icons.pin_drop,
-                                          color: Colors.blueAccent,
-                                          size: 60,
+                                        builder: (context) => Icon(
+                                          Icons.person,
+                                          size: 50,
                                         ),
                                       ),
-                                    )
-                                  ],
+                                    ],
+                                  ),
+                                SuperclusterLayer.immutable(
+                                  initialMarkers: markersA,
+                                  indexBuilder: IndexBuilders.rootIsolate,
+                                  clusterWidgetSize: const Size(40, 40),
+                                  anchor: AnchorPos.align(AnchorAlign.center),
+                                  calculateAggregatedClusterData: true,
+                                  builder: (context, position, markerCount,
+                                      extraClusterData) {
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                        color: Colors.blue,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          markerCount.toString(),
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
                                 ),
                               ],
                             ),
@@ -292,16 +400,31 @@ class _NearMeScreenMapViewContent extends HookWidget {
                             child: Column(
                               children: [
                                 MapButton(
-                                  icon: Icons.center_focus_strong,
-                                  onClicked: () => mapController.animateTo(
-                                    dest: center.value,
-                                  ),
-                                ),
-                                const MapButton(
+                                    icon: Icons.center_focus_strong,
+                                    onClicked: () {
+                                      mapController.animateTo(
+                                        dest: geolocation.latLng,
+                                      );
+                                    }),
+                                MapButton(
                                   icon: Icons.zoom_in,
+                                  onClicked: () {
+                                    if (zoom.value > 17) {
+                                      return;
+                                    }
+                                    zoom.value = zoom.value + 1;
+                                    mapController.animatedZoomTo(zoom.value);
+                                  },
                                 ),
-                                const MapButton(
+                                MapButton(
                                   icon: Icons.zoom_out,
+                                  onClicked: () {
+                                    if (zoom.value < 10) {
+                                      return;
+                                    }
+                                    zoom.value = zoom.value - 1;
+                                    mapController.animatedZoomTo(zoom.value);
+                                  },
                                 ),
                               ],
                             ),

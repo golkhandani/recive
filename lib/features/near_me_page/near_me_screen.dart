1389020f64a4eef20cad23497a6bfd17b0ed8f34
golkhandani <site.golkhandani@gmail.com>
2023-08-12 20:21:40 -0700

    006 - add flutter map

--- lib/features/near_me_page/near_me_screen.dart
+++ lib/features/near_me_page/near_me_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:recive/components/sliver_gap.dart';
@@ -10,6 +11,9 @@ import 'package:recive/features/home_page/home_screen.dart';
 import 'package:recive/layout/context_ui_extension.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 import 'package:toggle_switch/toggle_switch.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PageViewKeepAlive extends StatefulWidget {
   const PageViewKeepAlive({super.key, required this.data});
@@ -32,23 +36,54 @@ class _PageViewKeepAliveState extends State<PageViewKeepAlive>
   }
 }
 
-class NearMeScreen extends HookWidget {
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NearMeScreen extends StatefulHookWidget {
   static const name = 'near_me';
   const NearMeScreen({super.key});
 
+  @override
+  State<NearMeScreen> createState() => _NearMeScreenState();
+}
+
+class _NearMeScreenState extends State<NearMeScreen>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     final switchIndex = useState(0);
     final pageController = usePageController(initialPage: switchIndex.value);
     final switchItems = ['Map', 'List'];
     const switchDuration = Duration(milliseconds: 300);
+    final mapController = AnimatedMapController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+      curve: Curves.easeInOut,
+    );
+
+    final points = <LatLng>[
+      LatLng(51.509324, -0.128128),
+      LatLng(51.502334, -0.128528),
+      LatLng(51.503214, -0.128128),
+      LatLng(51.504564, -0.128228),
+      LatLng(51.5055264, -0.125928),
+      LatLng(51.501164, -0.128428),
+      LatLng(51.502464, -0.128228),
+      LatLng(51.504564, -0.128328),
+    ];
 
     return ColoredBox(
       color: Colors.amber[100]!,
       child: LayoutBuilder(builder: (context, box) {
         final contentHeight = box.maxHeight - context.invisibleHeight - 24 - 32;
-        final mapSectionHeight = (contentHeight * 0.65) - 24 - 12;
-        final listSectionHeight = (contentHeight * 0.35) - 24 - 12;
+        final mapSectionHeight = (contentHeight * 0.72) - 24 - 12;
+        final listSectionHeight = (contentHeight * 0.28) - 24 - 12;
         return CustomScrollView(
           physics: const NeverScrollableScrollPhysics(),
           slivers: [
@@ -105,12 +140,16 @@ class NearMeScreen extends HookWidget {
                           switchItems: switchItems,
                           mapSectionHeight: mapSectionHeight,
                           listSectionHeight: listSectionHeight,
+                          mapController: mapController,
+                          items: points,
                         ),
                       ],
                     ),
-                    const CustomScrollView(
+                    CustomScrollView(
                       slivers: [
-                        _NearMeScreenListViewContent(),
+                        _NearMeScreenListViewContent(
+                          items: points,
+                        ),
                       ],
                     ),
                   ],
@@ -124,15 +163,18 @@ class NearMeScreen extends HookWidget {
   }
 }
 
-class _NearMeScreenListViewContent extends StatelessWidget {
-  const _NearMeScreenListViewContent();
+class _NearMeScreenListViewContent extends HookWidget {
+  const _NearMeScreenListViewContent({
+    required this.items,
+  });
+  final List<LatLng> items;
 
   @override
   Widget build(BuildContext context) {
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 12).copyWith(bottom: 112),
       sliver: SliverList.builder(
-          itemCount: 10,
+          itemCount: items.length,
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.only(bottom: 12),
@@ -145,6 +187,7 @@ class _NearMeScreenListViewContent extends StatelessWidget {
                     borderRadius: BorderRadius.circular(8),
                     color: Colors.blueAccent,
                   ),
+                  child: Center(child: Text(items[index].toString())),
                 ),
               ),
             );
@@ -153,21 +196,26 @@ class _NearMeScreenListViewContent extends StatelessWidget {
   }
 }
 
-class _NearMeScreenMapViewContent extends StatelessWidget {
+class _NearMeScreenMapViewContent extends HookWidget {
   const _NearMeScreenMapViewContent({
+    required this.items,
     required this.switchIndex,
     required this.switchItems,
     required this.mapSectionHeight,
     required this.listSectionHeight,
+    required this.mapController,
   });
 
   final ValueNotifier<int> switchIndex;
   final List<String> switchItems;
   final double mapSectionHeight;
   final double listSectionHeight;
+  final AnimatedMapController mapController;
+  final List<LatLng> items;
 
   @override
   Widget build(BuildContext context) {
+    final center = useState(LatLng(51.509364, -0.128928));
     return SliverPadding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
       sliver: MultiSliver(
@@ -185,6 +233,82 @@ class _NearMeScreenMapViewContent extends StatelessWidget {
                     ),
                     width: sliver.crossAxisExtent,
                     height: mapSectionHeight,
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: FlutterMap(
+                              mapController: mapController.mapController,
+                              options: MapOptions(
+                                center: center.value,
+                                zoom: 15,
+                                adaptiveBoundaries: false,
+                                keepAlive: true,
+                              ),
+                              nonRotatedChildren: [
+                                RichAttributionWidget(
+                                  attributions: [
+                                    TextSourceAttribution(
+                                      'OpenStreetMap contributors',
+                                      onTap: () => launchUrl(Uri.parse(
+                                        'https://openstreetmap.org/copyright',
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                              children: [
+                                TileLayer(
+                                  urlTemplate:
+                                      'https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=J4ktALZX8GCz9Hw7i0tK',
+                                ),
+                                MarkerLayer(
+                                  markers: [
+                                    ...List.generate(
+                                      items.length,
+                                      (index) => Marker(
+                                        point: items[index],
+                                        width: 80,
+                                        height: 80,
+                                        builder: (context) => const Icon(
+                                          Icons.pin_drop,
+                                          color: Colors.blueAccent,
+                                          size: 60,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                MapButton(
+                                  icon: Icons.center_focus_strong,
+                                  onClicked: () => mapController.animateTo(
+                                    dest: center.value,
+                                  ),
+                                ),
+                                const MapButton(
+                                  icon: Icons.zoom_in,
+                                ),
+                                const MapButton(
+                                  icon: Icons.zoom_out,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               }),
@@ -208,71 +332,22 @@ class _NearMeScreenMapViewContent extends StatelessWidget {
                         enableInfiniteScroll: true,
                         padEnds: true,
                         slideIndicator: const CircularSlideIndicator(),
+                        onPageChanged: (index, reason) {
+                          center.value = items[index];
+                          mapController.animateTo(dest: items[index]);
+                        },
                       ),
-                      itemCount: 10 + 1,
-                      itemBuilder: (context, i, index) => Padding(
+                      itemCount: items.length,
+                      itemBuilder: (context, index, _) => Padding(
                         padding: EdgeInsets.symmetric(horizontal: 6),
-                        child: index == 10 + 1
-                            ? Container(
-                                width: sliver.asBoxConstraints().maxWidth / 2,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: context.theme.colorScheme.secondary,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    "See More!",
-                                    style:
-                                        context.textTheme.titleMedium!.copyWith(
-                                      color: context.theme.colorScheme
-                                          .onSecondaryContainer,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            : Container(
-                                width: sliver.asBoxConstraints().maxWidth / 1.1,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Colors.blueAccent,
-                                ),
-                              ),
-                      ),
-                    ),
-                  );
-                  return SliverToBoxAdapter(
-                    child: SizedBox(
-                      height: listSectionHeight,
-                      child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 11,
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(width: 12),
-                        itemBuilder: (context, index) => index == 10
-                            ? Container(
-                                width: sliver.asBoxConstraints().maxWidth / 2,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: context.theme.colorScheme.secondary,
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    "See More!",
-                                    style:
-                                        context.textTheme.titleMedium!.copyWith(
-                                      color: context.theme.colorScheme
-                                          .onSecondaryContainer,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            : Container(
-                                width: sliver.asBoxConstraints().maxWidth / 1.1,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Colors.blueAccent,
-                                ),
-                              ),
+                        child: Container(
+                          width: sliver.asBoxConstraints().maxWidth / 1.1,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.blueAccent,
+                          ),
+                          child: Center(child: Text(items[index].toString())),
+                        ),
                       ),
                     ),
                   );
@@ -282,6 +357,48 @@ class _NearMeScreenMapViewContent extends StatelessWidget {
           ),
           const SliverGap(height: 32),
         ],
+      ),
+    );
+  }
+}
+
+class MapButton extends StatelessWidget {
+  const MapButton({
+    Key? key,
+    required this.icon,
+    this.onClicked,
+  }) : super(key: key);
+
+  final IconData icon;
+  final VoidCallback? onClicked;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Material(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          hoverColor: Colors.orange,
+          splashColor: Colors.black,
+          focusColor: Colors.yellow,
+          highlightColor: Colors.amber,
+          onTap: onClicked,
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            width: 48,
+            height: 48,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            alignment: Alignment.center,
+            child: Icon(
+              icon,
+              color: Colors.black,
+              size: 25,
+            ),
+          ),
+        ),
       ),
     );
   }
